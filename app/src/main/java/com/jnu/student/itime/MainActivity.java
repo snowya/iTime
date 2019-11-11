d5d6aeae8ae38cb8b13333a4681b118684f402dc
Muzi <841474151@qq.com>
2019-11-11 17:23:42 +0800

    Initial commit

---
+++ app/src/main/java/com/jnu/student/itime/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jnu.student.itime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
