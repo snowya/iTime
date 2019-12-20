4bc62c94d13bb1f96eb7740d8fbde688ccd6783f
Muzi <841474151@qq.com>
2019-12-21 01:32:20 +0800

    主题色的修改

---
+++ app/src/main/java/com/jnu/student/myapplication/ColorSeekBar.java
@@ -0,0 +1,149 @@
+package com.jnu.student.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ColorSeekBar extends View {
+
+    private final Paint paint = new Paint();
+    private final Path sPath = new Path();
+    private float sLeft, sTop;
+    private float sWidth,sHeight;
+    private float x,y;
+    private float mRadius;
+    private int startColor=Color.BLACK;
+    private int endColor=Color.WHITE;
+    private int[] colorArray;
+    private OnColorSelectedListener onColorSelectedListener=null;
+
+    public ColorSeekBar(Context context) {
+        this(context, null);
+    }
+
+    public void setCircleX (float x) {
+        this.x =x;
+        this.invalidate();
+    }
+
+    public void setColor(int startColor,int endColor, int ...colors){
+        colorArray=new int[colors.length+2];
+        this.startColor=startColor;
+        this.endColor=endColor;
+        colorArray[0]=startColor;
+        System.arraycopy(colors,0,colorArray,1,colors.length);
+        colorArray[colors.length+1]=endColor;
+    }
+
+    public ColorSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthSize = (int)(MeasureSpec.getSize(widthMeasureSpec)*0.85);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec)*2;
+        setMeasuredDimension(widthSize, heightSize);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        float sRight, sBottom;
+        mRadius=h/2;
+        sLeft = mRadius; // 背景左的坐标
+        sTop = h*0.4f;//top位置
+        sRight = w-mRadius; // 背景的宽的全部
+        sBottom = h*0.6f; // 背景底部
+        sWidth = sRight - sLeft; // 背景的宽度
+        sHeight = sBottom - sTop; // 背景的高度
+        sPath.moveTo(sLeft,sTop+sHeight/2);
+        sPath.lineTo(sWidth,sTop+sHeight/2);
+        sPath.close();    // path准备背景的路径
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawBackground(canvas);
+        drawCircle(canvas);
+        paint.reset();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public boolean onTouchEvent(MotionEvent event) {
+        this.x = event.getX();
+        x=x<0?0:x;//判断thumb边界
+        x=x>sWidth?sWidth:x;
+        if (onColorSelectedListener!=null) {
+            onColorSelectedListener.onColorSelected(getColor(x), x);
+        }
+        this.invalidate();
+        return true;
+    }
+
+    // 画圆形
+    private void drawCircle(Canvas canvas){
+        Paint thumbPaint = new Paint();
+        x=x<mRadius?mRadius:x;//判断thumb边界
+        x=x>sWidth-mRadius?sWidth-mRadius:x;
+        RadialGradient radialGradient = new RadialGradient(x,mRadius,mRadius,getColor(x), Color.WHITE,Shader.TileMode.CLAMP);
+        thumbPaint.setStyle(Style.FILL);
+        thumbPaint.setShader(radialGradient);
+        canvas.drawCircle(x, mRadius, mRadius, thumbPaint);
+    }
+
+    private void drawBackground(Canvas canvas){
+        LinearGradient linearGradient=new LinearGradient(sLeft,sTop,sWidth,sHeight,colorArray,null, Shader.TileMode.REPEAT);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        //设置渲染器
+        paint.setShader(linearGradient);
+        paint.setStrokeWidth(6);
+        canvas.drawPath(sPath, paint);
+    }
+
+    //回调接口
+    public interface OnColorSelectedListener {
+        void onColorSelected(int color, float x);
+    }
+
+    public void setOnColorSelectedListener(OnColorSelectedListener onColorSelectedListener) {
+        this.onColorSelectedListener = onColorSelectedListener;
+    }
+
+    //x为seekBar位移,获取某一个百分比间的颜色,radio取值[0,1]
+    public int getColor(float x) {
+        float width = sWidth;
+        float period = (int)(width/(colorArray.length-1));
+        int start = (int)(x/period);
+        start=start>=colorArray.length-1?start-1:start;
+        int colorStart = colorArray[start];
+        int colorEnds = colorArray[start+1];
+
+        float radio = (x-period*start)/period;
+        int redStart = Color.red(colorStart);
+        int blueStart = Color.blue(colorStart);
+        int greenStart = Color.green(colorStart);
+        int redEnd = Color.red(colorEnds);
+        int blueEnd = Color.blue(colorEnds);
+        int greenEnd = Color.green(colorEnds);
+
+        int red = (int) (redStart + ((redEnd - redStart) * radio + 0.5));
+        int greed = (int) (greenStart + ((greenEnd - greenStart) * radio + 0.5));
+        int blue = (int) (blueStart + ((blueEnd - blueStart) * radio + 0.5));
+        return Color.argb(255,red, greed, blue);
+    }
+}
+
--- app/src/main/java/com/jnu/student/myapplication/MainActivity.java
+++ app/src/main/java/com/jnu/student/myapplication/MainActivity.java
@@ -1,30 +1,50 @@
 package com.jnu.student.myapplication;
 
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import android.view.MenuItem;
 import android.view.View;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
+import com.jnu.student.myapplication.home.HomeFragment;
 
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public static final int REQUEST_CODE = 902;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final int REQUEST_CODE_NEW_EVENT = 902;
+    public static final int REQUEST_CODE_UPDATE_EVENT = 903;
+    public static final int REQUEST_CODE_EDIT_EVENT = 904;
     private AppBarConfiguration mAppBarConfiguration;
+    // 主题颜色
+    private int themeColor = Color.parseColor("#03A9F4");
+    private int changingColor = Color.parseColor("#03A9F4");
+    // seekBar的进度
+    private float circleX = 0;
+    public static NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, NewEventActivity.class);
-                startActivityForResult(intent, REQUEST_CODE);
+                intent.putExtra("color", themeColor);
+                startActivityForResult(intent, REQUEST_CODE_NEW_EVENT);
             }
         });
 
@@ -51,14 +72,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        // 设置侧边导航栏选项及其跳转
+        // 设置侧边导航栏选项
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_color)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -73,5 +95,99 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_color) {
+            AlertDialog.Builder seekBarBuilder = new AlertDialog.Builder(this);
+            seekBarBuilder.setTitle("选择主题色");
+            seekBarBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    themeColor = changingColor;
+                    dialog.dismiss();
+                }
+            });
+            seekBarBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    setThemeColor();
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = seekBarBuilder.create();
+            View dialogView = View.inflate(this, R.layout.dialog_color_seekbar, null);
+            ColorSeekBar seekBar = dialogView.findViewById(R.id.seekBar);
+            seekBar.setOnColorSelectedListener(new ColorSeekBar.OnColorSelectedListener() {
+                @Override
+                public void onColorSelected(int color, float x) {
+                    circleX = x;
+                    changingColor = color;
+                    changeThemeColor(color);
+                }
+            });
+            if (circleX != 0)
+                seekBar.setCircleX(circleX);
+            seekBar.setColor(Color.RED, Color.BLACK, Color.BLUE, Color.GREEN, Color.YELLOW);
+            dialog.setView(dialogView);
+            dialog.show();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_NEW_EVENT:
+                if (resultCode == RESULT_OK) {
+                    navController.navigate(R.id.nav_home);
+                }
+                break;
+        }
+    }
+
+    // 设置为主题色
+    public void setThemeColor() {
+        // 修改悬浮按钮的颜色
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(themeColor));
+        // 修改标题栏的颜色
+        Toolbar toolbar = findViewById(R.id.event_activity_toolbar);
+        toolbar.setBackgroundColor(themeColor);
+    }
+
+    // 改变主题色
+    public void changeThemeColor(int color) {
+        // 修改悬浮按钮的颜色
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(color));
+        // 修改标题栏的颜色
+        Toolbar toolbar = findViewById(R.id.event_activity_toolbar);
+        toolbar.setBackgroundColor(color);
+    }
+
+    // 字符串转换为日期
+    static public Date stringToDate(String s) {
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+        try {
+            //字符串转日期
+            return sdf.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // 日期转化为字符串；formatType格式为yyyy-MM-dd HH:mm:ss//yyyy年MM月dd日 HH时mm分ss秒
+    public static String dateToString(Date data, String formatType) {
+        return new SimpleDateFormat(formatType).format(data);
     }
 }
